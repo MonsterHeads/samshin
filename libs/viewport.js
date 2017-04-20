7e7e5057c46d57f0dcea4878378d17a7464b377c
Jicheol Ryu <jicheol@monsterheads.com>
2017-04-20 04:24:55 +0200

    private 변수명 변경. scale 적용.

--- libs/viewport.js
+++ libs/viewport.js
@@ -20,40 +20,55 @@ var LoadingScene = function() {
 }
 
 var Viewport = function(init_config) {
-	var scene;
-	var config;
-	var ctx;
+	var _scene;
+	var _config;
+	var _ctx;
 
 	var init = function() {
 		var default_config = {'fps': 15, 'width': 640, 'height': 480,}
-		config = $.extend({}, default_config, init_config);
+		_config = $.extend({}, default_config, init_config);
 		var canvas = $('<canvas></canvas>');
 		canvas.css('width', '100%');
 		canvas.css('height', '100%');
-		canvas.attr('width', config.width);
-		canvas.attr('height', config.height);
-		config.target.append(canvas);
-		ctx = canvas[0].getContext('2d');
-		ctx.mozImageSmoothingEnabled = false;
-		ctx.webkitImageSmoothingEnabled = false;
-		ctx.msImageSmoothingEnabled = false;
-		ctx.imageSmoothingEnabled = false;
+		canvas.attr('width', _config.width);
+		canvas.attr('height', _config.height);
+		_config.target.append(canvas);
+		_config.render_width = Math.ceil(_config.width/config.viewport.scale);
+		_config.render_height = Math.ceil(_config.height/config.viewport.scale);
+
+		_ctx = canvas[0].getContext('2d');
+		_ctx.scale(config.viewport.scale, config.viewport.scale);
+		_ctx.mozImageSmoothingEnabled = false;
+		_ctx.webkitImageSmoothingEnabled = false;
+		_ctx.msImageSmoothingEnabled = false;
+		_ctx.imageSmoothingEnabled = false;
+
+		$(window).on('keydown', function(evt) {
+			if( _scene && typeof _scene.eventCallback == 'function' ) {
+				_scene.eventCallback('keydown', evt);
+			}
+		});
+		$(window).on('keyup', function(evt) {
+			if( _scene && typeof _scene.eventCallback == 'function' ) {
+				_scene.eventCallback('keyup', evt);	
+			}
+		});
 		render();
 	}
 	var render = function() {
 		var t = new Date().getTime();
-		if( scene ) {
-			scene.render(t, ctx, config.width, config.height);
+		if( _scene ) {
+			_scene.render(t, _ctx, _config.render_width, _config.render_height);
 		} else {
-			ctx.fillStyle = "#000000";
-			ctx.fillRect(0, 0, config.width, config.height);
+			_ctx.fillStyle = "#000000";
+			_ctx.fillRect(0, 0, _config.width, _config.height);
 		}
-		setTimeout(function(){render();}, 1000/config.fps);
+		setTimeout(function(){render();}, 1000/_config.fps);
 	}
 
 	init();
 	
 	this.setScene = function(new_scene) {
-		scene = new_scene;
+		_scene = new_scene;
 	};
 };
