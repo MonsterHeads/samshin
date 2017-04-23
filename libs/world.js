f00ad4a9f22306c9d95904acf9bc1fc37dc50b44
Jicheol Ryu <jicheol@monsterheads.com>
2017-04-23 14:41:48 +0200

    Scene방식 수정 - data에서 scene관련 스크립트를 삽입하도록.

--- libs/world.js
+++ libs/world.js
@@ -1,18 +1,27 @@
-var World = function(ground_tiles, map_data) {
+var World = function(ground_tiles, map_data, scene_name) {
 	var _grid_width = map_data.width;
 	var _grid_height = map_data.height;
 	var _width = _grid_width * config.ground_tile.unit;
 	var _height = _grid_height * config.ground_tile.unit;
 	var _ground_data = $.extend({}, map_data.ground);
+	var _center_x = _width/2;
+	var _center_y = _height/2;
 
 	this.getWidth = function(){return _width;}
 	this.getHeight = function(){return _height;}
-
-	this.render = function(ctx, width, height, center_x, center_y) {
-		var map_src_x = Math.max(0, center_x-width/2);
-		var ctx_dst_x = Math.max(0, width/2-center_x);
-		var map_src_y = Math.max(0, center_y-height/2);
-		var ctx_dst_y = Math.max(0, height/2-center_y);
+	this.setCenter = function(x, y) {
+		_center_x = x;
+		_center_y = y;
+	};
+	this.eventCallback = function(type, evt) {
+		scene.eventCallback(type, evt);
+	};
+	this.render = function(t, ctx, width, height) {
+		scene.doAction(t, width, height);
+		var map_src_x = Math.max(0, _center_x-width/2);
+		var ctx_dst_x = Math.max(0, width/2-_center_x);
+		var map_src_y = Math.max(0, _center_y-height/2);
+		var ctx_dst_y = Math.max(0, height/2-_center_y);
 		var real_width = Math.max(0, Math.min(width-ctx_dst_x, _width-map_src_x));
 		var real_height = Math.max(0, Math.min(height-ctx_dst_y, _height-map_src_y));
 		var init_cur_x = ctx_dst_x + 0-(map_src_x%config.ground_tile.unit);
@@ -29,42 +38,14 @@ var World = function(ground_tiles, map_data) {
 		for( var y=init_y; y<to_y; y++ ) {
 			var cur_x = init_cur_x;
 			for( var x=init_x; x<to_x; x++ ) {
-				ground_tiles.getGroundTile(_ground_data[y][x]).draw(ctx, cur_x, cur_y);
+				var tile_idx = _ground_data[y][x];
+				if( 0 < tile_idx ) {
+					ground_tiles.getGroundTile(tile_idx).draw(ctx, cur_x, cur_y);
+				}
 				cur_x += config.ground_tile.unit;
 			}
 			cur_y += config.ground_tile.unit;
 		}
 	}
-};
-
-var WorldCameraScene = function(world) {
-	var _center_x = world.getWidth()/2;
-	var _center_y = world.getHeight()/2;
-
-	var _dx = 0;
-	var _dy = 0;
-	this.eventCallback = function(type, evt) {
-		switch(type) {
-		case 'keydown':
-			switch(evt.keyCode) {
-			case 37: _dx -= 1; break;
-			case 38: _dy -= 1; break;
-			case 39: _dx += 1; break;
-			case 40: _dy += 1; break;
-			}
-			switch(evt.keyCode) {
-			case 37: case 38: case 39: case 40: evt.preventDefault();
-			}
-			break;
-		}
-	};
-	this.render = function(t, ctx, width, height) {
-		_center_x += _dx*2;
-		_center_y += _dy*2;
-		_center_x = Math.min(width/2, Math.max(_center_x, world.getWidth()-width/2));
-		_center_y = Math.min(height/2, Math.max(_center_y, world.getHeight()-height/2));
-		world.render(ctx, width, height, _center_x, _center_y);
-		_dx = 0;
-		_dy = 0;
-	};
+	var scene = new map_data.scenes[scene_name](this);
 };
--- resources/data/maps.js
+++ resources/data/maps.js
@@ -1,16 +1,60 @@
 var data_maps = {
 'room1': {
-	'width':7, 'height':7,
+	'width':11, 'height':14,
 	'map_object': [
 	],
 	'ground':[
-	[201,202,202,202,202,202,203],
-	[204,101,101,101,101,101,205],
-	[204,101,101,101,101,101,205],
-	[204,101,101,101,101,101,205],
-	[204,101,101,101,101,101,205],
-	[204,101,101,101,101,101,205],
-	[206,207,207,207,207,207,208],
+	[201,202,202,202,202,202,202,202,202,202,203],
+	[204,215,215,215,215,215,215,215,215,215,205],
+	[204,215,215,215,215,215,215,215,215,215,205],
+	[204,216,216,216,216,216,216,216,216,216,205],
+	[204,101,101,101,101,101,101,101,101,101,205],
+	[204,101,101,101,101,101,101,101,101,101,205],
+	[204,101,101,101,101,101,101,101,101,101,205],
+	[204,101,101,101,101,101,101,101,101,101,205],
+	[204,101,101,101,101,101,101,101,101,101,205],
+	[204,101,101,101,101,101,101,101,101,101,205],
+	[204,101,101,101,101,101,101,101,101,101,205],
+	[206,207,207,209,101,101,101,210,207,207,208],
+	[ -1, -1, -1,204,101,101,101,205, -1, -1, -1],
+	[ -1, -1, -1,211,207,207,207,212, -1, -1, -1],
 	],
+
+
+	'scenes': {
+'default': function(world) {
+	var _center_x = world.getWidth()/2;
+	var _center_y = world.getHeight()/2;
+	var _dx = 0;
+	var _dy = 0;
+	var eventCallback = function(type, evt) {
+		switch(type) {
+		case 'keydown':
+			switch(evt.keyCode) {
+			case 37: _dx -= 1; break;
+			case 38: _dy -= 1; break;
+			case 39: _dx += 1; break;
+			case 40: _dy += 1; break;
+			}
+			switch(evt.keyCode) {
+			case 37: case 38: case 39: case 40: evt.preventDefault();
+			}
+			break;
+		}
+	}
+	var doAction = function(t, view_width, view_height) {
+		_center_x += _dx*2;
+		_center_y += _dy*2;
+		_center_x = Math.min(view_width/2, Math.max(_center_x, world.getWidth()-view_width/2));
+		_center_y = Math.min(view_height/2, Math.max(_center_y, world.getHeight()-view_height/2));
+		world.setCenter(_center_x, _center_y);
+		_dx = 0;
+		_dy = 0;
+	};
+	return {'eventCallback': eventCallback, 'doAction': doAction};
+},
+	}
+
+
 }
 };
