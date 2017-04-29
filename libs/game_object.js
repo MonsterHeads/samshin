ee3b1612c3ff7dc73e3ba3c3b1393b494c660136
Jicheol Ryu <jicheol@monsterheads.com>
2017-04-29 08:15:29 +0200

    GameObject에 z값 추가 및 위치 변화에 따른 그리는 순서 변경 구현. GameObject의 Observer 기본 구현

--- libs/game_object.js
+++ libs/game_object.js
@@ -5,18 +5,70 @@ var GameObject = function(gameObjectPool, assets, data, initialData) {
 	var _boxData = {'width':0, 'height':0, 'hitboxList':[]};
 	var _x = initialData.x;
 	var _y = initialData.y;
+	var _z = 0;
+	if( initialData.hasOwnProperty('z') ) {
+		_z = initialData.z;
+	}
 
 	var _data = {};
 	var _status;
 	var _statusStartTime = -1;
 	var _childMap = {};
 	var _childList = [];
-	this.setChild = function(child_name, child) {
-		_childList.push(child);
-		_childMap[child_name] = {'inst':child, 'idx':_childList.length-1};
+	var _observeTypes = {'position':{}, 'size':{}, 'status':{},};
+	var _observerMap = {}; // GroupMap > TypeMap > InstanceList
+
+	this.setChild = function(childName, child) {
+		var insertIdx = 0;
+		for(var idx=0; idx<_childList.length; idx++) {
+			var childWrap = _childList[idx];
+			if( child.z > childWrap.inst.z  ) continue;
+			if( child.z < childWrap.inst.z  ) break;
+			if( child.y + child.height < childWrap.inst.y + childWrap.inst.height ) break;
+		}
+		insertIdx = idx;
+		_childList.splice(insertIdx, 0, {'inst':child, 'name':childName});
+		_childMap[childName] = {'inst':child, 'idx':insertIdx};
+		child.removeObserverGroup('__parent');
+		child.addObserver('__parent', {
+			'valueChanged': function(object, propertyName, before, after) {
+				switch(propertyName) {
+				case 'y': case 'height': case 'z':
+				if( before > after ) {
+					var curIdx = _childMap[childName].idx;
+					for(var idx=curIdx-1; idx>=0; idx--) {
+						var childWrap = _childList[idx];
+						if( child.z < childWrap.inst.z  ) continue;
+						if( child.z > childWrap.inst.z  ) break;
+						if( child.y + child.height >= childWrap.inst.y + childWrap.inst.height ) break;
+					}
+					if( idx+1 != curIdx ) {
+						var cur = _childList[curIdx];
+						_childMap[childName].idx = idx+1;
+						_childList.splice(curIdx,1);
+						_childList.splice(idx+1,0, cur);
+					}
+				} else {
+					var curIdx = _childMap[childName].idx;
+					for(var idx=curIdx+1; idx<_childList.length; idx++) {
+						var childWrap = _childList[idx];
+						if( child.z > childWrap.inst.z  ) continue;
+						if( child.z < childWrap.inst.z  ) break;
+						if( child.y + child.height < childWrap.inst.y + childWrap.inst.height ) break;
+					}
+					if( idx-1 != curIdx ) {
+						var cur = _childList[curIdx];
+						_childMap[childName].idx = idx-1;
+						_childList.splice(curIdx,1);
+						_childList.splice(idx-1,0, cur);
+					}
+				}
+				}
+			}
+		});
 	};
-	this.child = function(child_name) {
-		return _childMap[child_name].inst;
+	this.child = function(childName) {
+		return _childMap[childName].inst;
 	};
 	Object.defineProperty(this, 'status', {
 		'get': function() { return _status; },
@@ -27,6 +79,12 @@ var GameObject = function(gameObjectPool, assets, data, initialData) {
 			_status = status;
 			_statusStartTime = -1;
 			var statusBoxData = _statusMap[_status].init.apply($this,[assets]);
+			if( statusBoxData.hasOwnProperty('width') ) {
+				fireObserveEvent('size', 'width', _boxData.width, statusBoxData.width);
+			}
+			if( statusBoxData.hasOwnProperty('height') ) {
+				fireObserveEvent('size', 'height', _boxData.height, statusBoxData.height);
+			}
 			$.extend(_boxData, statusBoxData);			
 		},
 	});
@@ -36,11 +94,15 @@ var GameObject = function(gameObjectPool, assets, data, initialData) {
 	});
 	Object.defineProperty(this, 'x', {
 		'get':function() { return _x; },
-		'set':function(x) { _x = x;},
+		'set':function(x) { fireObserveEvent('position', 'x', _x, x); _x = x;},
 	});
 	Object.defineProperty(this, 'y', {
 		'get':function() { return _y; },
-		'set':function(y) { _y = y;},
+		'set':function(y) { fireObserveEvent('position', 'y', _y, y); _y = y;},
+	});
+	Object.defineProperty(this, 'z', {
+		'get':function() { return _z; },
+		'set':function(z) { fireObserveEvent('position', 'z', _z, z); _z = z;},
 	});
 	Object.defineProperty(this, 'width', {
 		'get':function() { return _boxData.width; },
@@ -51,6 +113,44 @@ var GameObject = function(gameObjectPool, assets, data, initialData) {
 	Object.defineProperty(this, 'hitboxList', {
 		'get':function() { return _boxData.hitboxList; },
 	});
+	var fireObserveEvent = function(type, propertyName, before, after) {
+		if( before === after ) return;
+		$.each(_observeTypes[type], function(group) {
+			$.each(_observerMap[group][type], function(idx, observer) {
+				setTimeout(function(){observer.valueChanged($this, propertyName, before, after);}, 0);
+			});
+		});
+	};
+	this.addObserver = function(group, observer, types) {
+		var typeToObserve = {};
+		if( !types || types.length == 0 ) types = Object.keys(_observeTypes);
+		for( var i=0; i<types.length; i++ ) {
+			if( _observeTypes.hasOwnProperty(types[i]) ) {
+				typeToObserve[types[i]] = true;
+			}
+		}
+		if( !_observerMap.hasOwnProperty(group) ) {
+			_observerMap[group] = {};
+		}
+		var typeMap = _observerMap[group];
+		$.each(typeToObserve, function(type) {
+			_observeTypes[type][group] = true;
+			if( !typeMap.hasOwnProperty(type) ) {
+				typeMap[type] = [];
+			}
+			typeMap[type].push(observer);
+		});
+	};
+	this.removeObserverGroup = function(group) {
+		if( _observerMap.hasOwnProperty(group) ) {
+			delete _observerMap[group];
+		}
+		$.each(_observeTypes, function(type, groupMap) {
+			if( groupMap.hasOwnProperty(group) ) {
+				delete groupMap[group];
+			}
+		});
+	}
 
 	this.hitCheck = function(otherObject) {
 		var o = otherObject;
@@ -71,9 +171,9 @@ var GameObject = function(gameObjectPool, assets, data, initialData) {
 	this.hitCheckWithChildren = function(gameObject) {
 		if( 0 == gameObject.hitboxList.length ) return false;
 		var result = false;
-		$.each(_childList, function(idx, child) {
-			if( child.hitCheck(gameObject) ) {
-				result = child;
+		$.each(_childList, function(idx, childWrap) {
+			if( childWrap.inst.hitCheck(gameObject) ) {
+				result = childWrap.inst;
 				return false;
 			}
 		});
@@ -85,9 +185,15 @@ var GameObject = function(gameObjectPool, assets, data, initialData) {
 		}
 		var statusData = _statusMap[_status];
 		var statusBoxData = statusData.beforeRender.apply($this, [t-_statusStartTime]);
+		if( statusBoxData.hasOwnProperty('width') ) {
+			fireObserveEvent('size', 'width', _boxData.width, statusBoxData.width);
+		}
+		if( statusBoxData.hasOwnProperty('height') ) {
+			fireObserveEvent('size', 'height', _boxData.height, statusBoxData.height);
+		}
 		$.extend(_boxData, statusBoxData);
-		$.each(_childList, function(idx, child) {
-			child.beforeRender(t);
+		$.each(_childList, function(idx, childWrap) {
+			childWrap.inst.beforeRender(t);
 		});
 	}
 	this.render = function(t, ctx) {
@@ -105,10 +211,10 @@ var GameObject = function(gameObjectPool, assets, data, initialData) {
 			ctx.closePath();
 		});
 
-		$.each(_childList, function(idx, child) {
+		$.each(_childList, function(idx, childWrap) {
 			ctx.save();
-			ctx.translate(child.x, child.y);
-			child.render(t, ctx);
+			ctx.translate(childWrap.inst.x, childWrap.inst.y);
+			childWrap.inst.render(t, ctx);
 			ctx.restore();
 		});
 	};
