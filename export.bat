88d021571d1fca12e43156110ca270b6906baab8
Jicheol Ryu <jicheol@monsterheads.com>
2017-05-21 02:20:53 +0200

    export 해야 하는 것 추가.

--- export.bat
+++ export.bat
@@ -6,8 +6,10 @@ rd . /S /Q
 md resources
 cd resources
 md images
+md font
 cd ..
 cd ..
 xcopy resources\images export\resources\images /e /h /k
+xcopy resources\font export\resources\font /e /h /k
 copy export.html export\index.html
-uglifyjs jquery.min.js libs\*.js resources\data\*.js > export\g.js
+uglifyjs jquery.min.js libs\*.js resources\data\*.js resources\scripts\*.js > export\g.js
